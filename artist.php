096bdf82485a1c4148f3b4f6696f5d52c9cb7af1
William Fritz <fritzw3@students.rowan.edu>
2022-12-07 13:13:27 -0500

    changes

--- artist.php
+++ artist.php
@@ -17,6 +17,11 @@
 
 <html lang="en">
     <head>
+    <style>
+body {
+  background: rgb(255,246,223);
+}
+        </style>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -30,13 +35,13 @@
         </form>
     </head>
     <body>
-        <h1><?= $artist['artist_name']; ?></h1><br>
-        <h2><?= $artist['artist_name']; ?> has <?= number_format($artist['listeners']) ?> listeners</h2>
+        <h1 style="background-color:rgb(199, 197, 252);"><center><?= $artist['artist_name']; ?></center></h1><br>
+        <h2><center><?= $artist['artist_name']; ?> has <?= number_format($artist['listeners']) ?> listeners</center></h2>
 
-        <img src="<?= $artist['artist_image'] ?>" height=200px width=200px>
-        <h2><?= $artist['artist_name']; ?>'s most popular song is <?= strstr($artist['youtube_title'], '- '); ?></h2>
-        <iframe width="420" height="315" src="https://www.youtube.com/embed/<?= $artist['videoID']; ?>"></iframe>
-        <h5><?= $artist['youtube_description'] ?></h5>
+        <center><img src="<?= $artist['artist_image'] ?>" height=200px width=200px></center>
+        <h2><center><?= $artist['artist_name']; ?>'s most popular song is <?= strstr($artist['youtube_title'], '- '); ?></center></h2>
+        <center><iframe width="420" height="315" src="https://www.youtube.com/embed/<?= $artist['videoID']; ?>"></iframe></center>
+        <h5><center><?= $artist['youtube_description'] ?></center></h5>
     </body>
     <script src="./includes/script.js"></script>
 </html>
